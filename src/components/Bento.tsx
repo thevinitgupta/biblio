f5358c3d538537b2e06ec8aaf47b41c1222ddd95
Vinit Gupta <thevinitgupta@gmail.com>
2025-02-09 18:00:06 +0530

    update globals.css

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -7,7 +7,7 @@ const Bento = () => {
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 h-full">
                     <div className="col-span-2 sm:col-span-1 md:col-span-2  h-auto md:h-full flex flex-col">
                         <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
-                            <Image src={"/sleeping.svg"} alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" objectFit="cover" layout="fill" />
+                            <Image src={"/sleeping.svg"} alt="" className="absolute inset-0 h-max-full w-max-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out " objectFit="cover" layout="fill" />
                             <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
                             <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Connect</h3>
                         </a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,16 @@ const config: Config = {
         // 'victor-mono': ['Victor Mono'],
         // poppins: ['Poppins'],
       },
+      keyframes : {
+        'border-spin' : {
+          '100%' : {
+            transform : 'rotate(-360deg)'
+          },
+        },
+      },
+      animation: {
+        'border-spin' : 'border-spin 7s linear infinite'
+      }
     },
   },
   daisyui: {
